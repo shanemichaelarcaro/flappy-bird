bc6223f4ce0f71cc2fdf3cdf30909f9ea2c3fc7a
Shane Arcaro <smarcaro@gmail.com>
2020-08-26 19:12:47 -0400

    Adding transition from title screen to game

--- sketch.js
+++ sketch.js
@@ -1,11 +1,33 @@
 let playable = false;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(624, 624);
 }
 
 function draw() {
+  if (!playable)
+    return;
   background(220);
 }
 
-console.log('Testing 2');
+function startGame() {
+  playable = true;
+
+  hideMainElements(true);
+}
+
+function hideMainElements(display) {
+  let normalDisplay = display ? 'hidden' : 'block';
+  let oppositeDisplay = display ? 'block' : 'hidden';
+
+  let canvas = document.getElementById('defaultCanvas0');
+  let menuItems = document.getElementsByClassName('menu-screen');
+
+  for (let i = 0; i < menuItems.length; i++) {
+    console.log(i, menuItems[i]);
+    menuItems[i].style.display = 'none';
+  }
+
+  canvas.style.display = oppositeDisplay;
+}
+
