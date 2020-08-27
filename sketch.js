747cc7e6d3a78e9234c513baa1ac11414f814105
Shane Arcaro <smarcaro@gmail.com>
2020-08-27 12:51:58 -0400

    Deleting old assets / finishing bird jump and gravity mechanics

--- sketch.js
+++ sketch.js
@@ -1,13 +1,38 @@
 let playable = false;
+let images = {};
+let bird;
 
 function setup() {
-  createCanvas(624, 624);
+  createCanvas(312, 624);
+  bird = new Bird((width - images.downflap.width) / 2, (height - images.downflap.height) / 2, [images.downflap, images.midflap, images.upflap]);
 }
 
 function draw() {
   if (!playable)
     return;
-  background(220);
+
+  clear();
+  imageMode(CORNER);
+  image(images.background, 0, 0, width);
+  image(images.base, 0, height - 112);
+  bird.rotateRender();
+  if (bird.playing) {
+    bird.update();
+  }
+}
+
+function update() {
+  console.log('HELLO!');
+}
+
+function preload() {
+  images.background = loadImage('assets/background.png');
+  images.base = loadImage('assets/base.png');
+  images.downflap = loadImage('assets/downflap.png');
+  images.midflap = loadImage('assets/midflap.png');
+  images.upflap = loadImage('assets/upflap.png');
+  images.downpipe = loadImage('assets/downpipe.png');
+  images.uppipe = loadImage('assets/uppipe.png');
 }
 
 function startGame() {
@@ -17,17 +42,22 @@ function startGame() {
 }
 
 function hideMainElements(display) {
-  let normalDisplay = display ? 'hidden' : 'block';
-  let oppositeDisplay = display ? 'block' : 'hidden';
+  let normalDisplay = display ? 'none' : 'block';
+  let oppositeDisplay = display ? 'block' : 'none';
 
   let canvas = document.getElementById('defaultCanvas0');
   let menuItems = document.getElementsByClassName('menu-screen');
 
-  for (let i = 0; i < menuItems.length; i++) {
-    console.log(i, menuItems[i]);
-    menuItems[i].style.display = 'none';
-  }
+  for (let i = 0; i < menuItems.length; i++) 
+    menuItems[i].style.display = normalDisplay;
 
   canvas.style.display = oppositeDisplay;
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    bird.jump();
+    bird.playing = true;
+  }
 }
 
